6ec7f1d758fa526b705d80eec043b43613f4e5fb
Faisal Abdul Hamid <faisal.abdulhamid@gmail.com>
2018-01-02 20:55:21 +0700

    tambahan

--- config/app.php
+++ config/app.php
@@ -78,20 +78,20 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'id',
 
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
     |--------------------------------------------------------------------------
     |
-    | The fallback locale determines the locale to use when the current one
+    | The fallback locale determines the locale to use whid the current one
     | is not available. You may change the value to correspond to any of
     | the language folders that are provided through your application.
     |
     */
 
-    'fallback_locale' => 'en',
+    'fallback_locale' => 'id',
 
     /*
     |--------------------------------------------------------------------------
