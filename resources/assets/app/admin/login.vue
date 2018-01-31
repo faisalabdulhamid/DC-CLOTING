700ef988f9463c68c86116512ae22aed8eb3b6db
Faisal Abdul Hamid <faisal.abdulhamid@gmail.com>
2018-01-31 21:55:39 +0700

    commit url

--- resources/assets/app/admin/login.vue
+++ resources/assets/app/admin/login.vue
@@ -98,7 +98,7 @@
 		},
 		beforeCreate (){
 			if (this.$store.getters.isLoggedAdmin) {
-				window.location = 'http://dc-clothing.laravel/#/admin/home'
+				window.location = 'http://dc-clothing.com/#/admin/home'
 			}
 		}
 	}
